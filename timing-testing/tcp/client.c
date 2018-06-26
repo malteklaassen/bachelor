f76111209a5fe8268ca210dd2a00c9406a721375
Malte Klaassen <klaassen@mkla.de>
2018-06-26 07:35:54 +0200

    Add TCP test for fixing multiple restore issues

---
+++ timing-testing/tcp/client.c
@@ -0,0 +1,61 @@
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <criu/criu.h>
+
+int main(){
+    criu_init_opts();
+
+    const char * server_name = "localhost";
+    const int server_port = 8080;
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+
+    inet_pton(AF_INET, server_name, &server_addr.sin_addr);
+
+    server_addr.sin_port = htons(server_port);
+
+    int sock;
+    if ( ( sock = socket(AF_INET, SOCK_STREAM, 0) ) < 0 ) {
+        printf("Could not create socket.\n");
+        return 1;
+    }
+
+    if ( connect(sock, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0 ) {
+        printf("Could not connect to server.\n");
+        return 1;
+    }
+
+    int dir = open("/tmp/criu/client/", O_DIRECTORY);
+    criu_set_tcp_established(true);
+    criu_set_images_dir_fd(dir);
+    criu_set_shell_job(true);
+    criu_set_log_file("client.log");
+    criu_set_log_level(4);
+
+    criu_dump();
+
+
+    char buffer[4096] = "This is some data to be send to the server.\n";
+    send(sock, buffer, strlen(buffer), 0);
+
+    int n = recv(sock, buffer, 4096, 0);
+    if ( n < 0 ) {
+        printf("Could not read data from socket.\n");
+            return 1;
+    } else if ( n == 0 ) {
+        printf("EOF.\n");
+    } else {
+        write(STDOUT_FILENO, buffer, n);
+    }
+    close(sock);
+    return 0;
+}
---
+++ timing-testing/tcp/server.c
@@ -0,0 +1,88 @@
+#include <errno.h>
+#include <string.h>
+#include <unistd.h>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <time.h>
+
+#include <criu/criu.h>
+
+int SERVER_PORT = 8080;
+
+int main(){
+    criu_init_opts();
+
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    
+    server_addr.sin_port = htons(SERVER_PORT);
+    server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+
+    int listen_sock;
+    if ( (listen_sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("Could not create listen socket.\n");
+        return 1;
+    }
+
+    if ( (bind(listen_sock, (struct sockaddr *)&server_addr, sizeof(server_addr))) < 0 ) {
+        printf("Could not bind listen socket.\n");
+        return 1;
+    }
+
+    int wait_size = 1;
+
+    if ( listen(listen_sock, wait_size) < 0 ) {
+        printf("Could not open socket for listening.\n");
+        return 1;
+    }
+
+    struct sockaddr_in client_addr;
+    unsigned client_addr_len = 0;
+
+    //while ( 1 ) {
+        int sock;
+        if ( ( sock = accept(listen_sock, (struct sockaddr*) &client_addr, &client_addr_len)) < 0 ) {
+            printf("Could not open accept socket.\n");
+            return 1;
+        }
+
+        printf("Client connected with addr %s.\n", inet_ntoa(client_addr.sin_addr));
+        
+        int dir = open("/tmp/criu/server/", O_DIRECTORY);
+        criu_set_tcp_established(true);
+        criu_set_images_dir_fd(dir);
+        criu_set_shell_job(true);
+        criu_set_log_file("server.log");
+        criu_set_log_level(4);
+
+        criu_dump();
+        
+
+        char buffer[4096];
+        int n;
+        n = recv(sock, buffer, 4096, 0);
+        if ( n < 0 ) {
+            printf("Could not read data from socket.\n");
+            return 1;
+        } else if ( n == 0 ) {
+            printf("EOF.\n");
+        } else {
+            memset(buffer, 0, 4096);
+            snprintf(buffer, 4096, "%ld", time(NULL));
+            send(sock, buffer, n, 0);
+        }
+
+        close(sock);
+
+    //}
+
+    close(listen_sock);
+    return 0;
+}
